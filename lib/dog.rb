13e435b32293b8951f28016f0d0d92efe0f34f92
Laura Darlak <laura@polyporedesign.com>
2018-08-23 00:24:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,16 +3,16 @@ class Dog
 
   # writer method
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
 
   # reader method
   def name
-    this_dogs_name
+    @this_dogs_name
   end
 
 end
 
 lassie = Dog.new
 lassie.name = "Lassie"
-lassie.name
+puts lassie.name
